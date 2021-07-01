1ed83b80cbd9a5b5b59ca3bd9b64baffa63acb2e
ItsRafy <78569917+ItsRafy@users.noreply.github.com>
2021-07-01 00:49:00 -0400

    Add files via upload

---
+++ src/main/java/com/fabric/template/TemplateClient.java
@@ -0,0 +1,11 @@
+package com.fabric.template;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TemplateClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/fabric/template/TemplateMain.java
@@ -0,0 +1,11 @@
+package com.fabric.template;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TemplateMain implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
